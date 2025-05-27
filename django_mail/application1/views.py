4123b1b2599cfab18910cc962a2d5960613817cf
bhanuprasad423 <bhanubanu2101@gmail.com>
2025-05-27 11:20:04 +0530

    Add files via upload

---
+++ django_mail/application1/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.core.mail import send_mail
+from django.http import HttpResponse
+
+
+def django_mail(request):
+    send_mail(
+        subject='Hii How are you ',
+        message='Can we go out tommorow',
+        from_email='velpulabhanuprasad@gmail.com',
+        recipient_list=['vikasjadhav9505@gmail.com', 'bhanubanu2101@gmail.com', 'pathanmouserkhan786@gmail.com', 'shrabaniparida057@gmail.com'],
+        fail_silently=False,
+    )
+    return HttpResponse("Email sent successfully")
+
+ 
